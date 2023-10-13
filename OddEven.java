8d4b5691b7fbf372dece2d4cf4f9cf88fba9c60f
utsav <56678117+utsav-07@users.noreply.github.com>
2023-10-13 22:24:19 +0530

    Update OddEven.java
    user Input

--- OddEven.java
+++ OddEven.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class OddEven {
     public static void main(String args[]) {
-        int n = 10;
+        //user input
+         Scanner myObj = new Scanner(System.in);
+         int n = myObj.nextInt();
+       // int n = 10;
         int bitMask = 1;
         if((n & bitMask) == 0) {
             System.out.println("even");
