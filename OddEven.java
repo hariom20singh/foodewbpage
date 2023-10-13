ad4987886f541d0fb73c345a302f037e0837c34b
utsav <56678117+utsav-07@users.noreply.github.com>
2023-10-14 00:29:56 +0530

    Update OddEven.java
    odd even with user
     input

--- OddEven.java
+++ OddEven.java
@@ -1,11 +1,23 @@
 public class OddEven {
     public static void main(String args[]) {
-        int n = 10;
-        int bitMask = 1;
-        if((n & bitMask) == 0) {
-            System.out.println("even");
-        } else {
-            System.out.println("odd");
-        }
+
+        //odd even with ueser input
+
+         Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
+        // int n = 10;
+        // int bitMask = 1;
+        // if((n & bitMask) == 0) {
+        //     System.out.println("even");
+        // } else {
+        //     System.out.println("odd");
+        // }
     }
 }
