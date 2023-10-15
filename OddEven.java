04fa41ceb025346f89d050aaa7cc0a01221c07b3
AmiraKR <145571327+AmiraKR@users.noreply.github.com>
2023-10-15 12:32:12 +0530

    Update OddEven.java
    getting run time input in odd even program

--- OddEven.java
+++ OddEven.java
@@ -1,11 +1,17 @@
-public class OddEven {
-    public static void main(String args[]) {
-        int n = 10;
-        int bitMask = 1;
-        if((n & bitMask) == 0) {
-            System.out.println("even");
-        } else {
-            System.out.println("odd");
-        }
+import java.util.Scanner;
+
+public class EvenOdd {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
     }
 }
