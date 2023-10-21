c870fd3165142ce9c885e87ff1d402acf579605c
GMKmani <91533361+GMKmani@users.noreply.github.com>
2023-10-21 16:19:25 +0530

    Create NthrootBinarysearch.cpp

---
+++ NthrootBinarysearch.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int func(int mid, int n, int m) {
+    long long ans = 1;
+    for (int i = 1; i <= n; i++) {
+        ans = ans * mid;
+        if (ans > m) return 2;
+    }
+    if (ans == m) return 1;
+    return 0;
+}
+
+int NthRoot(int n, int m) {
+    //Use Binary search on the answer space:
+    int low = 1, high = m;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int midN = func(mid, n, m);
+        if (midN == 1) {
+            return mid;
+        }
+        else if (midN == 0) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
+
+int main()
+{
+    int n = 3, m = 27;
+    int ans = NthRoot(n, m);
+    cout << "The answer is: " << ans << "\n";
+    return 0;
+}￼Enter
