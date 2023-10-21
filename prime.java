fe36a85a9862a9758572975ce837b0c61451a335
NIRMAL M <muthukumaresannirmal@gmail.com>
2023-10-21 16:40:55 +0530

    updated prime

--- prime.java
+++ prime.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PrimeNumber {
+public class prime {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
